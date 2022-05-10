887f55ac912a8c0151ffa121919c4bb3e90ffe04
marumonizi <mochi93kou@gmail.com>
2022-05-10 16:31:30 +0900

    add layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  root 'static_pages#home'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/access',  to: 'static_pages#access'
 end
