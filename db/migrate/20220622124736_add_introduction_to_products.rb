c7ca525044fd2dfb90c6aa2353fad965f5200aad
marumonizi <mochi93kou@gmail.com>
2022-07-12 17:31:42 +0900

    マイグレーションファイルを修正#3

--- db/migrate/20220622124736_add_introduction_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddIntroductionToProducts < ActiveRecord::Migration[6.1]
-  def change
-    add_column :products, :introduction, :text
-  end
-end
--- db/migrate/20220623030506_rename_images_to_product.rb
+++
@@ -1,5 +0,0 @@
-class RenameImagesToProduct < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :products, :image, :images
-  end
-end
